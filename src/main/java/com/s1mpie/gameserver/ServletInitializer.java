98909943709f5caa1e80ed9cd1ffa531c640127e
S1mpIe <54990505+S1mpIe@users.noreply.github.com>
2019-12-03 17:28:16 +0800

    Init...

---
+++ src/main/java/com/s1mpie/gameserver/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.s1mpie.gameserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GameserverApplication.class);
+    }
+
+}
